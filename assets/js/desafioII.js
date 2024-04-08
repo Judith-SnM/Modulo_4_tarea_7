7e6de58eaa67fa1a45cb33605bb7b5b424b8c64e
Judith <judith.sanmartin.s@gmail.com>
2024-04-08 15:17:01 -0400

    se muestran

--- assets/js/desafioII.js
+++ assets/js/desafioII.js
@@ -1,1 +1,27 @@
-console.log("hola")
+(async function() {
+    try {
+        const url ='https://randomuser.me/api/?results=10';
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        const persona = data.results;
+        const mostrarPersona = document.getElementById('user-data');
+        
+        persona.forEach(user => {
+            const imagen = document.createElement('img')
+            imagen.src = user.picture.large;
+            const nombreApellido = document.createElement('h3')
+            nombreApellido.textContent = `${user.name.first} ${user.name.last}`;
+            const email = document.createElement('p')
+            email.textContent = `${user.email}`
+            const telefono = document.createElement('p')      
+            telefono.textContent = `${user.phone}`
+            mostrarPersona.appendChild(imagen);
+            mostrarPersona.appendChild(nombreApellido);
+            mostrarPersona.appendChild(email);
+            mostrarPersona.appendChild(telefono);
+        });
+    } catch (error) {
+        console.error('No se encuentra el usuarios:', error);
+    }
+})();
